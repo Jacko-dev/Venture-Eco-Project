03dab1a07608fc5c17d0983d9c395b84604f04c1
Jack <jack.okeeffedev@gmail.com>
2022-12-26 19:26:11 +1100

    commit

--- src/blockchain/blockchain.js
+++ src/blockchain/blockchain.js
@@ -15,6 +15,22 @@ class Blockchain {
     getPrevBlock = () => {
         return this.chain[this.chain.length - 1]
     }
+    addBlock = (block) => {
+        block.index = this.getPrevBlock().index + 1
+        block.prevHash = this.getPrevBlock().hash
+        block.hash = block.calcHash()
+        block.mineBlock(this.difficulty)
+
+        this.chain.push(Object.freeze(block))
+    }
+    addTransaction = (transaction) => {
+        this.transactions.push(Object.freeze(transaction))
+    }
+    mineTransactions = () => {
+        this.addBlock(new Block(this.transactions, null))
+
+        this.transactions = []
+    }
 }
 
 module.exports = {
